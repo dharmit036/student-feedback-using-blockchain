b034797842c9dd00d34037c6b13c6b907dd35187
Dharmit Saradva <46236383+dharmit036@users.noreply.github.com>
2019-07-20 16:31:39 +0530

    Add files via upload

---
+++ src/BB/check.java
@@ -0,0 +1,188 @@
+package BB;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+import java.security.MessageDigest;
+import java.util.Date;
+
+@WebServlet("/check")
+public class check extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String FIRST=request.getParameter("FIRST");
+		String SECOND=request.getParameter("SECOND");
+		String THIRD=request.getParameter("THIRD");
+		PrintWriter out=response.getWriter();
+		/*out.println(FIRST);
+		out.println(SECOND);
+		out.println(THIRD);*/
+		Chain c1=new Chain();
+		String ALL = "Subject - [" + FIRST + " ] Faculty Name - [" + SECOND + " ]  Feedback - [" + THIRD + " ]" ;
+		out.println(c1.block(ALL));
+		
+	
+	}
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
+
+
+
+
+
+class Chain {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 1;
+
+	public String block(String all) {	
+		
+		
+		addBlock(new Block("-----------------------------", "0"));
+		addBlock(new Block(all,blockchain.get(blockchain.size()-1).hash));
+		
+		
+		String blockchainJson = StringUtil.getJson(blockchain);
+		
+		
+		return blockchainJson;
+	}
+	
+	public static Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+		
+		//loop through blockchain to check hashes:
+		for(int i=1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+				System.out.println("Current Hashes not equal");			
+				return false;
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	public static void addBlock(Block newBlock) {
+		newBlock.mineBlock(difficulty);
+		blockchain.add(newBlock);
+	}
+}
+
+
+class Block {
+	
+	public String hash;
+	public String previousHash; 
+	private String data; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+	private int nonce;
+	
+	//Block Constructor.  
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+	}
+	
+	//Calculate new hash based on blocks contents
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				data 
+				);
+		return calculatedhash;
+	}
+	
+	//Increases nonce value until hash target is reached.
+	public void mineBlock(int difficulty) {
+		String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+		
+	}
+	
+}
+
+
+
+class StringUtil {
+	
+	//Applies Sha256 to a string and returns the result. 
+	public static String applySha256(String input){
+		
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	        
+			//Applies sha256 to our input, 
+			byte[] hash = digest.digest(input.getBytes("UTF-8"));
+	        
+			StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//Short hand helper to turn Object into a json string
+	public static String getJson(Object o) {
+		return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+	}
+	
+	//Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"  
+	public static String getDificultyString(int difficulty) {
+		return new String(new char[difficulty]).replace('\0', '0');
+	}
+	
+	
+}
+
+
+
+
+
